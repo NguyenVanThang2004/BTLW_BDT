08de974413dcd4ffe05a8fd11e11eaf5115d3bad
phamthang2k4 <phamthang6636@gmail.com>
2024-10-23 16:52:36 +0700

    update Cart and Checkout

---
+++ Controllers/CheckoutController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BTLW_BDT.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        public IActionResult DetailCheckout()
+        {
+            ViewData["Page"] = "Checkout";
+            return View();
+        }
+    }
+}
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,157 +8,35 @@ namespace BTLW_BDT.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly BtlLtwQlbdtContext _context;
+        BtlLtwQlbdtContext db = new BtlLtwQlbdtContext();
 
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(BtlLtwQlbdtContext context, ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
-            _context = context;
+            
         }
 
-        public int CartCount
+
+        public int CartCount()
         {
-            get
-            {
-                return Carts.Count(); // Đếm tổng số lượng sản phẩm trong giỏ
-            }
+            var cart = HttpContext.Session.Get<List<CartItem>>("GioHang") ?? new List<CartItem>();
+            return cart.Count();
         }
-
         public IActionResult Index()
         {
-            ViewBag.CartCount = CartCount; // Truyền số lượng sản phẩm vào ViewBag
-            var lstSanPham = _context.SanPhams.ToList();
+            ViewBag.CartCount = CartCount(); // Truyền số lượng sản phẩm vào ViewBag
+            var lstSanPham = db.SanPhams.ToList();
        
             return View(lstSanPham);
         }
 
-        public List<CartItem> Carts
-        {
-            get
-            {
-                var data = HttpContext.Session.Get<List<CartItem>>("GioHang");
-                if (data == null)
-                {
-                    data = new List<CartItem>();
-                }
-                return data;
-            }
-        }
-
-
-        public IActionResult Cart()
-        {
-            ViewBag.CartCount = CartCount; // Truyền số lượng sản phẩm vào ViewBag
-            ViewData["Page"] = "Shopping Cart";
-            var cart = Carts;
-            
-            return View(Carts);
-        }
-
-        // Thêm sản phẩm vào giỏ hàng
-        public IActionResult AddToCart(string id)
-        {
-            var myCart = Carts;
-
-            // Tìm sản phẩm trong giỏ hàng hiện tại
-            var item = myCart.SingleOrDefault(x => x.MaSanPham == id);
-
-            if (item == null)
-            {
-                // Tìm sản phẩm trong CSDL
-                var hangHoa = _context.SanPhams.SingleOrDefault(x => x.MaSanPham == id);
-
-                if (hangHoa == null)
-                {
-                    // Xử lý trường hợp không tìm thấy sản phẩm trong CSDL
-                    return NotFound();
-                }
-
-
-
-
-                // Thêm sản phẩm vào giỏ hàng
-                item = new CartItem
-                {
-                    MaSanPham = id,  
-                    TenSanPham = hangHoa.TenSanPham,
-                    DonGia = hangHoa.DonGiaBanRa.HasValue ? hangHoa.DonGiaBanRa.Value : 0,  
-                    SoLuong = 1,
-                    Anh = hangHoa.AnhDaiDien  
-                };
-                myCart.Add(item);
-            }
-            else
-            {
-               
-                item.SoLuong++;
-            }
-
-            // Lưu giỏ hàng lại vào session
-            HttpContext.Session.Set("GioHang", myCart);
-
-           
-
-            return RedirectToAction("Index");  // Điều hướng về trang giỏ hàng
-        }
-
-
-        [HttpPost] 
-        public IActionResult RemoveFromCart(string maSanPham)
-        {
-            var myCart = Carts;
-
-            // Tìm sản phẩm trong giỏ hàng
-            var item = myCart.SingleOrDefault(x => x.MaSanPham == maSanPham);
-            if (item != null)
-            {
-                myCart.Remove(item); // Xóa sản phẩm
-            }
-
-            // Lưu giỏ hàng lại vào session
-            HttpContext.Session.Set("GioHang", myCart);
-            return RedirectToAction("Cart"); // Trở lại trang giỏ hàng
-        }
-
-        [HttpPost]
-        public IActionResult UpdateQuantity(string maSanPham, int quantity, string action)
-        {
-            var myCart = Carts;
-
-            // Tìm sản phẩm trong giỏ hàng
-            var item = myCart.SingleOrDefault(x => x.MaSanPham == maSanPham);
-            if (item != null)
-            {
-                // Tăng hoặc giảm số lượng dựa trên hành động
-                if (action == "increase")
-                {
-                    item.SoLuong++;
-                }
-                else if (action == "decrease")
-                {
-                    // Giảm số lượng, không cho phép số lượng < 1
-                    if (item.SoLuong > 1)
-                    {
-                        item.SoLuong--;
-                    }
-                }
-            }
-
-            // Lưu giỏ hàng lại vào session
-            HttpContext.Session.Set("GioHang", myCart);
-            return RedirectToAction("Cart"); // Trở lại trang giỏ hàng
-        }
-
+       
 
 
        
-        public IActionResult Checkout()
-        {
-            ViewData["Page"] = "Checkout";
-            return View();
-        }
+      
 
 
 
