939ca319919cdb71ed8e1debdb6140bc90f59ab3
Vann Kite <146878611+Kiteya@users.noreply.github.com>
2024-10-27 15:56:19 -0700

    Lọc theo giá.

--- Program.cs
+++ Program.cs
@@ -43,6 +43,6 @@ app.UseSession();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Shop}/{id?}");
 
 app.Run();
